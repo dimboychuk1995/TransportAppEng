c69505a8bdf486ca902d46651b1fd908263be240
yuralubinec <yuralubinec@gmail.com>
2016-10-25 10:11:15 +0300

    remove loging sql queries generated by hibernate

--- src/main/java/com/oblenergo/configuration/DatabaseConfiguration.java
+++ src/main/java/com/oblenergo/configuration/DatabaseConfiguration.java
@@ -38,8 +38,9 @@ public class DatabaseConfiguration {
 
         Properties properties = new Properties();
         properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+//        only for set-up
+//        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
         return properties;
     }
 
