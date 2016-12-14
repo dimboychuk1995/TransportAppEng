f0d8a437913878b0d9f03e5a118ccd97a695c4f6
Dmytro <dmytro.andrusiv@oe.if.ua>
2016-12-14 15:53:14 +0200

    added validation for phone number

--- src/main/java/com/oblenergo/validator/OrderValidator.java
+++ src/main/java/com/oblenergo/validator/OrderValidator.java
@@ -1,9 +1,13 @@
 package com.oblenergo.validator;
 
+import java.util.Collection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -12,7 +16,7 @@ import com.oblenergo.model.Orders;
 import com.oblenergo.service.SapService;
 
 @Component
-public class ClientValidator implements Validator {
+public class OrderValidator implements Validator {
 
   @Autowired
   SapService sapServiceImpl;
@@ -25,10 +29,11 @@ public class ClientValidator implements Validator {
 
   @Override
   public void validate(Object target, Errors errors) {
-
     String regexPattern = "([1][0-7]):[0-5][0-9]";
+    String regexPhoneNumber = "^\\+380([0-9]?){9,11}[0-9]$";
     Orders tn = (Orders) target;
     String tabNumber = tn.getUser_tab();
+    String phoneNumber = tn.getMobile_phone_number();
     String time = tn.getTime();
     String timeEnd = tn.getTime_end();
 
@@ -44,24 +49,43 @@ public class ClientValidator implements Validator {
       }
     }
 
-    if (!checkTime(regexPattern, time)) {
+    if (!checkInput(regexPattern, time)) {
       errors.rejectValue("time", "Bad.time");
     }
 
-    if (!checkTime(regexPattern, timeEnd)) {
+    if (!checkInput(regexPattern, timeEnd)) {
       errors.rejectValue("time_end", "Bad.time");
+
+    }
+
+    if (!checkInput(regexPhoneNumber, phoneNumber)) {
+      errors.rejectValue("mobile_phone_number", "Bad.phone.number");
+
     }
 
   }
 
-  public boolean checkTime(String regex, String time) {
+  public boolean checkInput(String regex, String input) {
     Pattern p = Pattern.compile(regex);
-    Matcher m = p.matcher(time);
+    Matcher m = p.matcher(input);
     if (m.find()) {
       return true;
     } else {
       return false;
     }
+  }
+
+  public boolean isAdmin() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    boolean isAdmin = false;
+    Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+    for (GrantedAuthority grantedAuthority : authorities) {
+      if (grantedAuthority.getAuthority().equals("ADMIN")) {
+        isAdmin = true;
+        break;
+      }
+    }
+    return isAdmin;
   }
 
 }
